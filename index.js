a9af445ca4ff1ef39723888cd61d914c713db4db
https://github.com/Emimayerk <emilianomayerviegas@gmail.com>
2024-02-22 19:06:56 -0300

    modificacion para agregar envio de datos de usuario en formulario

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const formDatas = document.querySelectorAll('.form-data');
+  const userInputs = document.querySelectorAll('.user-data');
 
   formDatas.forEach((formData) => {
     const inputs = formData.querySelectorAll('.input-data');
@@ -20,44 +21,52 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     });
   });
-});
 
-function prepararYEnviarPorWhatsApp() {
-  // Obtener la información de los resultados seleccionados
-  const resultados = [];
-  const formDatas = document.querySelectorAll('.form-data');
+  const updateForm = document.getElementById('submit-form');
 
-  formDatas.forEach((formData, index) => {
-    const inputs = formData.querySelectorAll('.input-data');
-    let resultadoSeleccionado = null;
+  updateForm.addEventListener('click', function () {
+    // Obtener la información de los resultados seleccionados
+    const resultados = [];
+    formDatas.forEach((formData, index) => {
+      const inputs = formData.querySelectorAll('.input-data');
+      let resultadoSeleccionado = null;
 
-    inputs.forEach((input) => {
-      if (input.style.backgroundColor === 'rgb(46, 46, 46)') {
-        resultadoSeleccionado = input.value;
-      }
+      inputs.forEach((input) => {
+        if (input.style.backgroundColor === 'rgb(46, 46, 46)') {
+          resultadoSeleccionado = input.value;
+        }
+      });
+
+      resultados.push(`Partido ${index + 1} = ${resultadoSeleccionado}`);
     });
 
-    resultados.push(`Partido ${index + 1} = ${resultadoSeleccionado}`);
+    // Obtener la información de los usuarios
+    const datosUsuario = obtenerDatosUsuario();
+
+    // Crear el mensaje de WhatsApp con los resultados y los datos del usuario
+    const mensajeWhatsApp = 'Mi Prode:\n' + resultados.join('\n') + '\n\nDatos del Usuario:\n' + datosUsuario.join('\n');
+
+    // Número de teléfono al que enviar el mensaje (reemplaza con el número deseado)
+    const numeroTelefono = '3465405225';
+
+    // Formatear el mensaje para la URL
+    const mensajeWhatsAppCodificado = encodeURIComponent(mensajeWhatsApp);
+
+    // Crear el enlace de WhatsApp
+    const enlaceWhatsApp = `https://wa.me/${numeroTelefono}?text=${mensajeWhatsAppCodificado}`;
+
+    // Abrir la pestaña de WhatsApp en una nueva ventana
+    window.open(enlaceWhatsApp, '_blank');
   });
 
-  // Crear el mensaje de WhatsApp con los resultados
-  const mensajeWhatsApp = 'Mi Prode:\n' + resultados.join('\n');
+  // Función para obtener los datos del usuario
+  function obtenerDatosUsuario() {
+    const datosUsuario = [];
 
-  // Número de teléfono al que enviar el mensaje (reemplaza con el número deseado)
-  const numeroTelefono = '3465405225';
+    userInputs.forEach((input) => {
+      datosUsuario.push(`${input.placeholder}: ${input.value}`);
+    });
 
-  // Formatear el mensaje para la URL
-  const mensajeWhatsAppCodificado = encodeURIComponent(mensajeWhatsApp);
-
-  // Crear el enlace de WhatsApp
-  const enlaceWhatsApp = `https://wa.me/${numeroTelefono}?text=${mensajeWhatsAppCodificado}`;
-
-  // Abrir la pestaña de WhatsApp en una nueva ventana
-  window.open(enlaceWhatsApp, '_blank');
-}
-
-const updateForm = document.getElementById('submit-form');
-
-updateForm.addEventListener('click', () => {
-  location.reload();
+    return datosUsuario;
+  }
 });
