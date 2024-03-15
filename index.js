f14f261450516fdb927717cb667446b84f9edda2
https://github.com/Emimayerk <emilianomayerviegas@gmail.com>
2024-03-15 17:45:32 -0300

    desactivar fecha

--- index.js
+++ index.js
@@ -1,72 +1,72 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const formDatas = document.querySelectorAll('.form-data');
-  const userInputs = document.querySelectorAll('.user-data');
+// document.addEventListener('DOMContentLoaded', function () {
+//   const formDatas = document.querySelectorAll('.form-data');
+//   const userInputs = document.querySelectorAll('.user-data');
 
-  formDatas.forEach((formData) => {
-    const inputs = formData.querySelectorAll('.input-data');
+//   formDatas.forEach((formData) => {
+//     const inputs = formData.querySelectorAll('.input-data');
 
-    inputs.forEach((input) => {
-      input.addEventListener('click', function () {
-        const selectedOption = input.value;
+//     inputs.forEach((input) => {
+//       input.addEventListener('click', function () {
+//         const selectedOption = input.value;
 
-        // Desmarcar todas las opciones en el mismo partido antes de marcar la actual
-        inputs.forEach((otherInput) => {
-          otherInput.style.backgroundColor = '';
-          otherInput.style.color = '';
-        });
+//         // Desmarcar todas las opciones en el mismo partido antes de marcar la actual
+//         inputs.forEach((otherInput) => {
+//           otherInput.style.backgroundColor = '';
+//           otherInput.style.color = '';
+//         });
 
-        // Marcar la opción actual
-        input.style.backgroundColor = '#45661a';
-        input.style.color = '#f0e7d6';
-      });
-    });
-  });
+//         // Marcar la opción actual
+//         input.style.backgroundColor = '#45661a';
+//         input.style.color = '#f0e7d6';
+//       });
+//     });
+//   });
 
-  const updateForm = document.getElementById('submit-form');
+//   const updateForm = document.getElementById('submit-form');
 
-  updateForm.addEventListener('click', function () {
-    // Obtener la información de los resultados seleccionados
-    const resultados = [];
-    formDatas.forEach((formData, index) => {
-      const inputs = formData.querySelectorAll('.input-data');
-      let resultadoSeleccionado = null;
+//   updateForm.addEventListener('click', function () {
+//     // Obtener la información de los resultados seleccionados
+//     const resultados = [];
+//     formDatas.forEach((formData, index) => {
+//       const inputs = formData.querySelectorAll('.input-data');
+//       let resultadoSeleccionado = null;
 
-      inputs.forEach((input) => {
-        if (input.style.backgroundColor === 'rgb(69, 102, 26)') {
-          resultadoSeleccionado = input.value;
-        }
-      });
+//       inputs.forEach((input) => {
+//         if (input.style.backgroundColor === 'rgb(69, 102, 26)') {
+//           resultadoSeleccionado = input.value;
+//         }
+//       });
 
-      resultados.push(`Partido ${index + 1} = ${resultadoSeleccionado}`);
-    });
+//       resultados.push(`Partido ${index + 1} = ${resultadoSeleccionado}`);
+//     });
 
-    // Obtener la información de los usuarios
-    const datosUsuario = obtenerDatosUsuario();
+//     // Obtener la información de los usuarios
+//     const datosUsuario = obtenerDatosUsuario();
 
-    // Crear el mensaje de WhatsApp con los resultados y los datos del usuario
-    const mensajeWhatsApp = 'Mi Prode:\n' + resultados.join('\n') + '\n\nDatos del Usuario:\n' + datosUsuario.join('\n');
+//     // Crear el mensaje de WhatsApp con los resultados y los datos del usuario
+//     const mensajeWhatsApp = 'Mi Prode:\n' + resultados.join('\n') + '\n\nDatos del Usuario:\n' + datosUsuario.join('\n');
 
-    // Número de teléfono al que enviar el mensaje (reemplaza con el número deseado)
-    const numeroTelefono = '+5493416725781';
+//     // Número de teléfono al que enviar el mensaje (reemplaza con el número deseado)
+//     const numeroTelefono = '+5493416725781';
 
-    // Formatear el mensaje para la URL
-    const mensajeWhatsAppCodificado = encodeURIComponent(mensajeWhatsApp);
+//     // Formatear el mensaje para la URL
+//     const mensajeWhatsAppCodificado = encodeURIComponent(mensajeWhatsApp);
 
-    // Crear el enlace de WhatsApp
-    const enlaceWhatsApp = `https://wa.me/${numeroTelefono}?text=${mensajeWhatsAppCodificado}`;
+//     // Crear el enlace de WhatsApp
+//     const enlaceWhatsApp = `https://wa.me/${numeroTelefono}?text=${mensajeWhatsAppCodificado}`;
 
-    // Abrir la pestaña de WhatsApp en una nueva ventana
-    window.open(enlaceWhatsApp, '_blank');
-  });
+//     // Abrir la pestaña de WhatsApp en una nueva ventana
+//     window.open(enlaceWhatsApp, '_blank');
+//   });
 
-  // Función para obtener los datos del usuario
-  function obtenerDatosUsuario() {
-    const datosUsuario = [];
+//   // Función para obtener los datos del usuario
+//   function obtenerDatosUsuario() {
+//     const datosUsuario = [];
 
-    userInputs.forEach((input) => {
-      datosUsuario.push(`${input.placeholder}: ${input.value}`);
-    });
+//     userInputs.forEach((input) => {
+//       datosUsuario.push(`${input.placeholder}: ${input.value}`);
+//     });
 
-    return datosUsuario;
-  }
-});
+//     return datosUsuario;
+//   }
+// });
