f78a3257df66a449cdb9ff95ad096e3722ff8679
jvl <jvl@MacBook-Pro-de-jvl.local>
2024-07-23 01:17:23 -0300

    wip show results

--- v1/vista/src/pages/home/index.tsx
+++ v1/vista/src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import { Fixture } from '../../components/fixture';
 import { PageWrapper } from '../../components/pageWrapper';
+import { PredictionEnum } from '../../enum/prediction';
 import { useProde } from '../../hooks/useProde';
 import { HomeProps } from '../../models/pages/home';
 
@@ -8,10 +9,21 @@ import './styles/index.scss';
 export const Home = ({ name, className }: HomeProps) => {
   const { prode } = useProde();
 
+  // TODO: Create buildResults util
+  const results = prode.map((match) => ({
+    match: `${match.local}-${match.visita}`,
+    prediction:
+      match.predict === PredictionEnum.LOCAL
+        ? match.local
+        : match.predict === PredictionEnum.VISITA
+          ? match.visita
+          : match.predict,
+  }));
+
   return (
     <PageWrapper name={name} className={className}>
       <Fixture matches={prode} />
-      <button onClick={() => alert(JSON.stringify(prode))}> JUGAR </button>
+      <button onClick={() => alert(JSON.stringify(results))}> JUGAR </button>
     </PageWrapper>
   );
 };
